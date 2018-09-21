77fa7260ce51b49ec3d586ff724532720ac2e3f7
Alison Hill <apreshill@gmail.com>
2018-09-21 15:04:49 -0700

    update

--- anonymize-students.R
+++ anonymize-students.R
@@ -2,21 +2,21 @@ library(tidyverse)
 source("generate-fake.R")
 set.seed(2018)
 
-student_names <- read_csv("/Users/hillali/Box Sync/CONJ 620 Grades/2018-08-16_lab-grades-tidy.csv") %>% 
+student_names <- read_csv("/Users/hillali/Box Sync/CONJ 620 Grades/2018-09-21_lab-grades-tidy.csv") %>% 
   select(name) %>% 
   distinct(name)
 
 student_names <- student_names %>% 
   bind_cols(make_fake_name(nrow(student_names)))
 
-sakai <- read_csv("/Users/hillali/Box Sync/CONJ 620 Grades/2018-08-16_lab-grades-tidy.csv") %>% 
+sakai <- read_csv("/Users/hillali/Box Sync/CONJ 620 Grades/2018-09-21_lab-grades-tidy.csv") %>% 
   left_join(student_names) %>% 
   select(-name, -email) 
   
-dc <- read_csv("/Users/hillali/Box Sync/CONJ 620 Grades/2018-08-16_datacamp-grades-tidy.csv") %>% 
+dc <- read_csv("/Users/hillali/Box Sync/CONJ 620 Grades/2018-09-21_datacamp-grades-tidy.csv") %>% 
   left_join(student_names) %>% 
   select(-name, -email)
 
-write_csv(student_names, "/Users/hillali/Box Sync/CONJ 620 Grades/name_key.csv")
+#write_csv(student_names, "/Users/hillali/Box Sync/CONJ 620 Grades/name_key.csv")
 write_csv(sakai, here::here("data", "sakai.csv"))
 write_csv(dc, here::here("data", "dc.csv"))
